e2aa2345d220ef1fbae8e8110ddbd3e7c95431f9
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-03-27 20:22:25 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// NOWPayments keys (kept private here only)
+define("NOWPAY_API_KEY", "EH6MW8E-28CMXAJ-PPCQ3GT-5EY8MWW");
+define("NOWPAY_PUBLIC_KEY", "7302d859-cff6-4202-9067-0da610a8a8f9");
+
+// Website info
+define("SUCCESS_URL", "https://buy-challange.onrender.com/success.html");
+define("CANCEL_URL", "https://buy-challange.onrender.com/index.html");
+
+?>
