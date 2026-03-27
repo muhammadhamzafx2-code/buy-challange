c95f60b78f659aa2ddbae725a6cb920a7fa4706b
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-03-27 20:47:55 +0100

    Update save_user.php

--- save_user.php
+++ save_user.php
@@ -1,19 +1,37 @@
 <?php
-$conn = new mysqli("localhost","root","","propfirm");
 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$dob = $_POST['dob'];
-$country = $_POST['country'];
-$balance = $_POST['balance'];
-$price = $_POST['price'];
-$type = $_POST['type'];
+$file = "users.json";
 
-$conn->query("INSERT INTO users(firstname,lastname,email,phone,dob,country,balance,price,type,status)
-VALUES('$firstname','$lastname','$email','$phone','$dob','$country','$balance','$price','$type','pending')");
+// get form data
+$data = [
+    "firstname" => $_POST['firstname'],
+    "lastname"  => $_POST['lastname'],
+    "email"     => $_POST['email'],
+    "phone"     => $_POST['phone'],
+    "dob"       => $_POST['dob'],
+    "country"   => $_POST['country'],
+    "balance"   => $_POST['balance'],
+    "price"     => $_POST['price'],
+    "type"      => $_POST['type'],
+    "status"    => "pending",
+    "date"      => date("Y-m-d H:i:s")
+];
 
-header("Location: create_payment.php?price=$price&email=$email&balance=$balance&type=$type");
+// read existing users
+if(file_exists($file)){
+    $current = json_decode(file_get_contents($file), true);
+}else{
+    $current = [];
+}
+
+// add new user
+$current[] = $data;
+
+// save back
+file_put_contents($file, json_encode($current, JSON_PRETTY_PRINT));
+
+// redirect to real payment page
+header("Location: create_payment.php?price=".$_POST['price']."&email=".$_POST['email']."&balance=".$_POST['balance']."&type=".$_POST['type']);
 exit;
+
 ?>
