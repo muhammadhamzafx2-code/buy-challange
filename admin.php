3ac8fb50249aadcdd4eab47c5d3f987d1ee23fed
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-03-27 20:48:34 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,15 +1,26 @@
 <?php
-$conn = new mysqli("localhost","root","","propfirm");
 
-$result = $conn->query("SELECT * FROM users ORDER BY id DESC");
+$file = "users.json";
 
-echo "<h2>Paid Users</h2>";
+if(!file_exists($file)){
+    echo "No users yet.";
+    exit;
+}
 
-while($row=$result->fetch_assoc()){
-echo $row['firstname']." ".
-$row['lastname']." - ".
-$row['email']." - ".
-$row['balance']." - $".
-$row['price']."<br><br>";
+$users = json_decode(file_get_contents($file), true);
+
+echo "<h2>Users Who Made Payment</h2>";
+
+foreach($users as $u){
+    echo "<div style='margin-bottom:20px'>";
+    echo "<b>Name:</b> ".$u['firstname']." ".$u['lastname']."<br>";
+    echo "<b>Email:</b> ".$u['email']."<br>";
+    echo "<b>Phone:</b> ".$u['phone']."<br>";
+    echo "<b>Country:</b> ".$u['country']."<br>";
+    echo "<b>Account:</b> $".$u['balance']." (".$u['type'].")<br>";
+    echo "<b>Price Paid:</b> $".$u['price']."<br>";
+    echo "<b>Date:</b> ".$u['date']."<br>";
+    echo "<hr>";
+    echo "</div>";
 }
 ?>
