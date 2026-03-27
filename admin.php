52a2ddc6caac8b6d8965295a79887f06a3d65ab3
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-03-27 20:25:28 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,1 +1,15 @@
+<?php
+$conn = new mysqli("localhost","root","","propfirm");
 
+$result = $conn->query("SELECT * FROM users ORDER BY id DESC");
+
+echo "<h2>Paid Users</h2>";
+
+while($row=$result->fetch_assoc()){
+echo $row['firstname']." ".
+$row['lastname']." - ".
+$row['email']." - ".
+$row['balance']." - $".
+$row['price']."<br><br>";
+}
+?>
