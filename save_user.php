224c0bf7d2196b292945f82278f1798edc36ba4e
muhammadhamzafx2-code <muhammadhamzafx2@gmail.com>
2026-03-27 22:31:00 +0100

    Update save_user.php

--- save_user.php
+++ save_user.php
@@ -2,36 +2,44 @@
 
 $file = "users.json";
 
-// get form data
+// make sure form was submitted
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    die("Invalid request");
+}
+
+// collect form data safely
 $data = [
-    "firstname" => $_POST['firstname'],
-    "lastname"  => $_POST['lastname'],
-    "email"     => $_POST['email'],
-    "phone"     => $_POST['phone'],
-    "dob"       => $_POST['dob'],
-    "country"   => $_POST['country'],
-    "balance"   => $_POST['balance'],
-    "price"     => $_POST['price'],
-    "type"      => $_POST['type'],
+    "firstname" => $_POST['firstname'] ?? "",
+    "lastname"  => $_POST['lastname'] ?? "",
+    "email"     => $_POST['email'] ?? "",
+    "phone"     => $_POST['phone'] ?? "",
+    "dob"       => $_POST['dob'] ?? "",
+    "country"   => $_POST['country'] ?? "",
+    "balance"   => $_POST['balance'] ?? "",
+    "price"     => $_POST['price'] ?? "",
+    "type"      => $_POST['type'] ?? "",
     "status"    => "pending",
     "date"      => date("Y-m-d H:i:s")
 ];
 
 // read existing users
-if(file_exists($file)){
+if (file_exists($file)) {
     $current = json_decode(file_get_contents($file), true);
-}else{
+    if (!is_array($current)) {
+        $current = [];
+    }
+} else {
     $current = [];
 }
 
 // add new user
 $current[] = $data;
 
-// save back
+// save users.json
 file_put_contents($file, json_encode($current, JSON_PRETTY_PRINT));
 
-// redirect to real payment page
-header("Location: create_payment.php?price=".$_POST['price']."&email=".$_POST['email']."&balance=".$_POST['balance']."&type=".$_POST['type']);
+// redirect to payment page
+header("Location: create_payment.php?price=".$data['price']."&balance=".$data['balance']."&type=".$data['type']);
 exit;
 
 ?>
